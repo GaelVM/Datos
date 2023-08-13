5fc6dc1b85e081acdd4048474582829964dd99d6
GaelVM <98351911+GaelVM@users.noreply.github.com>
2023-08-13 04:44:57 -0500

    asas

--- 1500Scrap.py
+++ 1500Scrap.py
@@ -36,30 +36,20 @@ for row in table.find_all("tr")[1:]:  # Ignorar la primera fila de encabezados
     cp = cells[7].text
     iv = cells[8].text
 
-    # Realizar solicitud para obtener los datos del tipo del Pokémon
-    type_url = f"https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex/id/{number_dex}.json"
-    type_response = requests.get(type_url)
+    # ... Código anterior ...
 
-    if type_response.status_code == 200:
-        type_data = type_response.json()
+    name_lower = name.lower()
+    if "galarian" in name_lower:
+        region_forms = type_data.get("regionForms", [])
+        if region_forms:
+            assets_galarian = region_forms[0].get("assets", {})
+            image_url = assets_galarian.get("image", image_url)
+            shiny_image_url = assets_galarian.get("shinyImage", shiny_image_url)
+    elif "shadow" in name_lower:
+        # Agrega aquí las URLs reales de las imágenes para Pokémon Shadow
+        image_url = "URL de la imagen para Pokémon Shadow"
+        shiny_image_url = "URL de la imagen shiny para Pokémon Shadow"
 
-        primary_type = type_data["primaryType"]["names"]["Spanish"]
-        secondary_type = type_data["secondaryType"]["names"]["Spanish"] if type_data["secondaryType"] else None
-
-        assets = type_data.get("assets", {})
-        image_url = assets.get("image", "N/A")
-        shiny_image_url = assets.get("shinyImage", "N/A")
-
-        name_lower = name.lower()
-        if "galarian" in name_lower:
-            region_forms = type_data.get("regionForms", [])
-            if region_forms:
-                image_url = region_forms[0].get("assets", {}).get("image", image_url)
-                shiny_image_url = region_forms[0].get("assets", {}).get("shinyImage", shiny_image_url)
-        elif "shadow" in name_lower:
-            # Agrega aquí las URLs reales de las imágenes para Pokémon Shadow
-            image_url = "URL de la imagen para Pokémon Shadow"
-            shiny_image_url = "URL de la imagen shiny para Pokémon Shadow"
     else:
         primary_type = "Desconocido"
         secondary_type = None
@@ -91,3 +81,6 @@ with open(output_file, "w") as json_file:
     json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 print(f"Se han raspado y guardado {len(data)} registros en {output_file}")
+
+   
+
