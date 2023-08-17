35ff093c2725207f4d0689eee3fc45d8597b031e
GaelVM <98351911+GaelVM@users.noreply.github.com>
2023-08-16 21:24:28 -0500

    sd

--- 1500Scrap.py
+++ 1500Scrap.py
@@ -28,7 +28,7 @@ for row in table.find_all("tr")[1:]:  # Ignorar la primera fila de encabezados
 
     number = cells[0].text
     name = cells[1].text
-    number = cells [2].text
+    number_dex = cells[2].text
     fast_skill = cells[3].text
     charged_skill_1 = cells[4].text
     charged_skill_2 = cells[5].text
@@ -36,24 +36,71 @@ for row in table.find_all("tr")[1:]:  # Ignorar la primera fila de encabezados
     cp = cells[7].text
     iv = cells[8].text
 
-    pokemon_data = {
-        "#": number,
-        "Name": name,
-        "NumberDex": number,
-        "Fast Skill": fast_skill,
-        "Charged Skill 1": charged_skill_1,
-        "Charged Skill 2": charged_skill_2,
-        "Level": level,
-        "CP": cp,
-        "IV": iv
-    }
+    # Obtener información adicional del Pokémon desde la API
+    number_dex_int = int(number_dex)
+    api_url = f"https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex/id/{number_dex}.json"
+    api_response = requests.get(api_url)
 
-    data.append(pokemon_data)
+    if api_response.status_code == 200:
+        api_data = api_response.json()
+        primary_type = api_data.get("primaryType", {}).get("names", {}).get("Spanish", "Desconocido")
+
+        secondary_type_data = api_data.get("secondaryType", {})
+        if secondary_type_data:
+            secondary_type = secondary_type_data.get("names", {}).get("Spanish", None)
+        else:
+            secondary_type = None
+
+        region_forms = api_data.get("regionForms", [])
+
+        image_url = "N/A"
+        shiny_image_url = "N/A"
+
+        name_lower = name.lower()
+        if "galarian" in name_lower:
+            assets_galarian = {}
+            for form_key, form_data in region_forms.items():
+                if "GALARIAN" in form_key:
+                    assets_galarian = form_data.get("assets", {})
+                    image_url = assets_galarian.get("image", image_url)
+                    shiny_image_url = assets_galarian.get("shinyImage", shiny_image_url)
+                    break  # Detener la búsqueda una vez que se encuentre la forma "GALARIAN"
+        
+        else:
+            assets_normal = api_data.get("assets", {})
+            image_url = assets_normal.get("image", image_url)
+            shiny_image_url = assets_normal.get("shinyImage", shiny_image_url)
+
+        if "shadow" in name_lower:
+            name = name.replace("Shadow", "Oscuro")
+            # Agrega aquí la URL real de la imagen adicional para Pokémon Shadow
+            additional_image_url = "URL de la imagen adicional para Pokémon Shadow"
+        else:
+            additional_image_url = None
+
+        pokemon_data = {
+            "#": number,
+            "Name": name,
+            "NumberDex": number_dex,
+            "Fast Skill": fast_skill,
+            "Charged Skill 1": charged_skill_1,
+            "Charged Skill 2": charged_skill_2,
+            "Level": level,
+            "CP": cp,
+            "IV": iv,
+            "primaryType": primary_type,
+            "secondaryType": secondary_type,
+            "image": image_url,
+            "shinyImage": shiny_image_url,
+            "additionalImage": additional_image_url
+        }
+
+        data.append(pokemon_data)
 
 # Guardar en formato JSON
 output_file = "pvp1500_data.json"
 
 with open(output_file, "w") as json_file:
-    json.dump(data, json_file, indent=4)
+    json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 print(f"Se han raspado y guardado {len(data)} registros en {output_file}")
