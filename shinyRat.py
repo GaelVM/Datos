a8beb0771f84f79c5dbc6b17e9852581183719b6
GaelVM <ajhuneor@outlook.com>
2023-10-06 12:17:49 -0500

    as

--- shinyRat.py
+++ shinyRat.py
@@ -1,42 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
 import json
 
-url = "https://shinyrates.com/"
+url = "https://shinyrates.com/data/rate/"
 
-# Realizamos la solicitud GET a la URL
 response = requests.get(url)
 
-# Comprobamos si la solicitud fue exitosa
 if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Encuentra la tabla con el id 'shiny_table'
-    table = soup.find('table', {'id': 'shiny_table'})
-    
-    data = []
-    
-    # Itera a través de las filas de la tabla
-    for row in table.find_all('tr'):
-        cols = row.find_all('td')
-        
-        # Verifica que haya al menos 5 columnas en la fila
-        if len(cols) >= 5:
-            item = {
-                'ID': cols[1].text.strip(),
-                'Name': cols[2].text.strip(),
-                'Shiny Rate': cols[3].text.strip(),
-                'Sample Size': cols[4].text.strip()
-            }
-            data.append(item)
-    
-    # Convierte los datos a formato JSON
-    json_data = json.dumps(data, indent=4)
-    
-    # Guarda el JSON en un archivo llamado ShinyRate.json
-    with open('ShinyRate.json', 'w') as json_file:
-        json_file.write(json_data)
-    
-    print("Los datos se han guardado en ShinyRate.json")
+    data = response.json()
+    formatted_data = []
+
+    for item in data:
+        formatted_item = {
+            "ID": item["id"],
+            "Name": item["name"],
+            "Shiny Rate": item["rate"],
+            "Sample Size": item["total"]
+        }
+        formatted_data.append(formatted_item)
+
+    # Ahora, puedes guardar formatted_data como JSON si lo deseas
+    with open("datos.json", "w") as file:
+        json.dump(formatted_data, file, indent=4)
+
 else:
-    print("No se pudo acceder a la página:", response.status_code)
+    print("No se pudo acceder al sitio web.")
