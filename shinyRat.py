3af148f6b332ac2a8d7f5d8d6bc7b9596da83617
GaelVM <ajhuneor@outlook.com>
2023-10-06 12:28:53 -0500

    sd

--- shinyRat.py
+++ shinyRat.py
@@ -21,8 +21,17 @@ if response.status_code == 200:
     # Ordenar los datos por el segundo valor en "Shiny Rate" en orden ascendente
     sorted_data = sorted(formatted_data, key=lambda x: int(x["Shiny Rate"].replace(",", "").split("/")[1]))
 
+    # Asignar un número secuencial a los elementos con "Shiny Rate" que contiene "1"
+    contador = 1
+    for item in sorted_data:
+        if "1" in item["Shiny Rate"]:
+            item["Top Number"] = contador
+            contador += 1
+        else:
+            item["Top Number"] = None
+
     # Ahora, puedes guardar sorted_data como JSON si lo deseas
-    with open("ShinyRatSorted.json", "w") as file:
+    with open("ShinyRat.json", "w") as file:
         json.dump(sorted_data, file, indent=4)
 
 else:
